a31034735a6ec1e5193fd05a205964b379646b3a
chenlongtoh <chenlongtoh@gmail.com>
2020-03-06 22:03:25 +0800

    Coments Added

--- map1/lib/main.dart
+++ map1/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Wrapper(),
+      home: Wrapper(), //Wrapper for switching branches
     );
   }
 }
--- map1/lib/screen/wrapper.dart
+++ map1/lib/screen/wrapper.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'home/home.dart';
 
 class Wrapper extends StatelessWidget {
+  //conditional return
+  //return home() if user is logged in
   @override
   Widget build(BuildContext context) {
     return Home();
