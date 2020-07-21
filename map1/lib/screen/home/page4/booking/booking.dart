58379edf62297c97bf784fd48f4e77b2fe9e4a1e
chenlongtoh <chenlongtoh@gmail.com>
2020-07-22 01:35:31 +0800

    Added Booking Views

---
+++ map1/lib/screen/home/page4/booking/booking.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:map1/services/database.dart';
+import 'package:map1/shared/constant.dart';
+import 'package:map1/shared/loading.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:provider/provider.dart';
+import 'package:map1/model/user.dart';
+import 'package:intl/intl.dart';
+import 'bookingList.dart';
+
+class BookingSchedule extends StatefulWidget {
+  @override
+  _BookingScheduleState createState() => _BookingScheduleState();
+}
+
+class _BookingScheduleState extends State<BookingSchedule> with TickerProviderStateMixin {
+  List _selectedEvents;
+  DateTime _selectedEventsDate;
+  AnimationController _animationController;
+  CalendarController _calendarController;
+  @override
+  void setState(fn) {
+    if (mounted) {
+      super.setState(fn);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Reset timestamp to 00:00:00
+    final str = DateFormat('yyyyMMdd').format(DateTime.now()).toString();
+    final _selectedDay = DateTime.parse(str);
+    _selectedEventsDate = _selectedDay;
+    _selectedEvents = [];
+    _calendarController = CalendarController();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    );
+
+    _animationController.forward();
+  }
+
+  void _onDaySelected(DateTime day, List events) {
+    setState(() {
+      _selectedEventsDate = day;
+      _selectedEvents = events;
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    _calendarController.dispose();
+    super.dispose();
+  }
+
+  Widget _buildTableCalendar(events) {
+    return TableCalendar(
+      daysOfWeekStyle: DaysOfWeekStyle(
+        weekdayStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        weekendStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+      ),
+      calendarController: _calendarController,
+      events: events,
+      holidays: {},
+      initialCalendarFormat: CalendarFormat.twoWeeks,
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      calendarStyle: CalendarStyle(
+        selectedStyle: TextStyle(color: blue),
+        selectedColor: blue2,
+        weekdayStyle: TextStyle(color: Colors.white.withOpacity(0.7), fontWeight: FontWeight.bold),
+        weekendStyle: TextStyle(color: Colors.white.withOpacity(1), fontWeight: FontWeight.bold),
+        todayColor: blue2.withOpacity(0.6),
+        markersColor: Colors.white,
+        outsideDaysVisible: false,
+      ),
+      headerStyle: HeaderStyle(
+        leftChevronIcon: Icon(Icons.keyboard_arrow_left, color: Colors.white),
+        rightChevronIcon: Icon(Icons.keyboard_arrow_right, color: Colors.white),
+        titleTextStyle: whiteReg_18,
+        centerHeaderTitle: true,
+        formatButtonVisible: false,
+        formatButtonTextStyle: whiteReg_16,
+        formatButtonDecoration: BoxDecoration(
+          color: Colors.pink,
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+      ),
+      onDaySelected: _onDaySelected,
+      onVisibleDaysChanged: null,
+      onCalendarCreated: null,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    final databaseService = DatabaseService(uid: user.uid);
+
+    return StreamBuilder(
+        stream: databaseService.bookingsInCalendarView,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData)
+            return Loading();
+          else {
+            final events = snapshot.data;
+            final str = DateFormat('yyyyMMdd').format(_selectedEventsDate).toString();
+            _selectedEventsDate = DateTime.parse(str);
+            _selectedEvents = events[_selectedEventsDate] ?? [];
+
+            return Scaffold(
+              appBar: AppBar(
+                centerTitle: true,
+                backgroundColor: blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(25),
+                    bottomRight: Radius.circular(25),
+                  ),
+                ),
+                title: Text(
+                  "Calendar",
+                  style: whiteBold_14,
+                ),
+                bottom: PreferredSize(
+                  preferredSize: Size(0, 170),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    height: 170,
+                    child: _buildTableCalendar(events),
+                  ),
+                ),
+              ),
+              body: Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: BookingList(
+                  bookings: _selectedEvents,
+                ),
+              ),
+            );
+          }
+        });
+  }
+}
---
+++ map1/lib/screen/home/page4/booking/bookingList.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:map1/shared/constant.dart';
+import 'package:map1/model/booking.dart';
+import 'package:intl/intl.dart';
+
+class BookingList extends StatelessWidget {
+  final List bookings;
+
+  BookingList({this.bookings});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      child: ListView.builder(
+        padding: EdgeInsets.only(left: 10, right: 10),
+        itemCount: bookings.length,
+        itemBuilder: (context, index) {
+          final booking = bookings[index];
+
+          String dateString = '';
+
+          // Append date and millisecond for parsing
+          final startTimeString = "20190123T" + booking.startTime + "00";
+
+          final duration = booking.bookedHour;
+          final startTime = DateTime.parse(startTimeString);
+          final endTime = startTime.add(new Duration(hours: duration));
+
+          dateString = DateFormat.jm().format(startTime).toString();
+          dateString += " - ";
+          dateString += DateFormat.jm().format(endTime).toString();
+
+          return Container(
+            decoration: BoxDecoration(
+              color: blue1,
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+            child: Slidable(
+              actionPane: SlidableDrawerActionPane(),
+              actionExtentRatio: 0.25,
+              child: ListTile(
+                contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                title: Text(
+                  "Hall: ${booking.hallName} \nCourt Number: ${booking.slotNumber}",
+                  style: eventText,
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top:10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(booking.bookingStatus),
+                      Text(dateString),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
